b7c90551eabe921cbadc8d5cd077ad117583e624
Krzysztof Jagoda <krzysztof.jagoda99@gmail.com>
2023-12-29 14:02:42 +0100

    Included information on how to get track's URL by using its ID

--- backend/data.ts
+++ backend/data.ts
@@ -4,3 +4,7 @@ export const tokenURL = "https://accounts.spotify.com/api/token";
 
 export const BASE_DELAY = 5000;
 export const MAX_RETRIES = 5;
+
+export const SPOTIFY_URL = "https://open.spotify.com/track/";
+
+//To get spotify URL of a track: SPOTIFY_URL + {trackId}
