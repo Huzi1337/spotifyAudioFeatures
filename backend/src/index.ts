9c74e901670c7642fb0651c3e70b8a1227107939
Krzysztof Jagoda <krzysztof.jagoda99@gmail.com>
2024-01-02 17:02:26 +0100

    Added quick and dirty frontend that can query server for data.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import audioFeatureRoutes from "./routes/audioFeatures.js";
+import cors from "cors";
 
 export const startServer = async () => {
   const PORT = 3000;
   const app = express();
+  app.use(cors());
   app.use(express.json());
 
   app.use("/api/v1/audioFeatures", audioFeatureRoutes);
